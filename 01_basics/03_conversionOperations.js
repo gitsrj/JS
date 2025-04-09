d5a6c4d4630bb73a24296f2a3d0975b4416ab2e9
Sahil Jaiswal <145742021+gitsrj@users.noreply.github.com>
2025-04-09 21:11:12 +0000

    Conversion of Data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,39 @@
+let score = "33abc"
+let age = null
+let marks = undefined
+
+let valueInNumber = Number(score)
+let valInNumber = Number(age);
+let numberVal = Number(marks);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// console.log(typeof valInNumber);
+// console.log(valInNumber);
+
+// console.log(typeof numberVal);
+// console.log(numberVal);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "xyz" => true
+
+let someNum = 33
+
+let stringNum = String(someNum)
+
+console.log(stringNum);
+console.log(typeof stringNum);
+

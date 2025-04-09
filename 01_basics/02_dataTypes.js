829124c049251f7b3decbef2c3e5ebe301c384d7
Sahil Jaiswal <145742021+gitsrj@users.noreply.github.com>
2025-04-09 20:54:27 +0000

    datattpes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS as newer version
+
+// alert(3 + 3)  // we are using node.js , not browser
+
+console.log(3
+    + 3  //code readability should be high
+)
+
+let name = "SRJ";
+let age = 20;
+let state;
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// bool => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);  //object
+

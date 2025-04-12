8e90043a1a0fb39431a6158aafcc44c84aa58d54
Sahil Jaiswal <145742021+gitsrj@users.noreply.github.com>
2025-04-12 19:55:57 +0000

    Memory in JS

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -12,9 +12,9 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id);
-console.log(anotherId);
-console.log(id == anotherId);
+// console.log(id);
+// console.log(anotherId);
+// console.log(id == anotherId);
 
 const bigNumber = 456789876523456n
 
@@ -34,6 +34,30 @@ const myFunction = function(){
     console.log("hello world");
 }
 
-console.log(typeof bigNumber);
-console.log(typeof myFunction);
+// console.log(typeof bigNumber);
+// console.log(typeof myFunction);
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non Primitive)
+
+let myName = "Srj"
+
+let anotherName = myName
+
+anotherName = "gitsrj"
+
+console.log(myName);
+console.log(anotherName)
+
+let userOne = {
+    email: "user@gmai.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "userTwo@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email);

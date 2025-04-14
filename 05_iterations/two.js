1139a1132ef7bb8e829a46c59235150739062477
Sahil Jaiswal <145742021+gitsrj@users.noreply.github.com>
2025-04-14 19:01:52 +0000

    Basic Loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,13 @@
+let i = 0
+
+// while (i <= 5) {
+//     console.log(`Value of index is ${i}`)
+//     i++
+// }
+
+let score = 1
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (false);
+

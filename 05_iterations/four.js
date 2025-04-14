76a3b9bc4a7f2eee814bbcf4d9bc6ba574460d95
Sahil Jaiswal <145742021+gitsrj@users.noreply.github.com>
2025-04-14 19:48:54 +0000

    Some Awesome Loops in JS

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift by apple',
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+const programming = ["js", "ruby", "python","cpp"]
+
+// for (const key in programming) {
+//     console.log(programming[key])
+// }
+
+
+const map = new Map()
+map.set("IN", "India")
+map.set("IN", "India")
+map.set("USA", "United Stated of America")
+map.set("FR", "France")
+
+// for (const key in map) {
+//     console.log(key)
+// }

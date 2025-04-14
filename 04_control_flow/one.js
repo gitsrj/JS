e9b7fcf25127c77a3e47cf69d612286f5f344b53
Sahil Jaiswal <145742021+gitsrj@users.noreply.github.com>
2025-04-14 18:39:18 +0000

    Control Flows and many more

---
+++ 04_control_flow/one.js
@@ -0,0 +1,25 @@
+// if
+const isUserLoggedIn = true
+
+
+if (isUserLoggedIn) {
+    
+}
+
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power: ${power}`)
+// }
+
+const balance = 1000
+
+// Aisa nahi karna
+
+// if(balance > 500) console.log("test"),
+// console.log("test 2")
+
+// &&, ||
